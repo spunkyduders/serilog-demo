e9f501fa1a898d98d2ef0fc60b6bf5c5ae4223d8
raj <spunkydude.rs@gmail.com>
2022-06-07 14:41:29 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5555"));
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
